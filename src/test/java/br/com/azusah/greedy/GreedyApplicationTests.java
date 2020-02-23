abcd8136a139013cc8c3f0e76a92ad1827cdb997
Daniel Luis Benedito Albino <daniel.albino@gmail.com>
2020-02-23 20:42:47 -0300

    Uploading the project.

---
+++ src/test/java/br/com/azusah/greedy/GreedyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.azusah.greedy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreedyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
