115f5c41114e30990f670babdbf0d16cc98a865d
Daniel Luis Benedito Albino <daniel.albino@gmail.com>
2020-02-26 21:06:34 -0300

    Improving the Openapi configurations.

--- src/main/java/br/com/azusah/greedy/framework/controllers/CourseController.java
+++ src/main/java/br/com/azusah/greedy/framework/controllers/CourseController.java
@@ -2,9 +2,17 @@ package br.com.azusah.greedy.framework.controllers;
 
 import br.com.azusah.greedy.boundary.ports.ICourseServicePort;
 import br.com.azusah.greedy.framework.controllers.resources.CourseResource;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,19 +34,29 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/courses")
 @RequiredArgsConstructor
-@Tag(name = "course", description = "A course resource.")
+@Tag(name = "Course", description = "A course resource.")
 public class CourseController {
 
     private final ICourseServicePort courseServicePort;
 
-    @PostMapping
+    @Operation(summary = "returns a resource saved on database.",
+            description = "Given a valid resource saves it on database and returns the object saved.",
+            tags = {"contact"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "resource created on database",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResource.class)))),
+            @ApiResponse(responseCode = "400", description = "Some data on resource body is invalid.")
+    })
     @ResponseStatus(HttpStatus.CREATED)
-    CourseResource insert(@Valid @RequestBody CourseResource courseResource) {
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    CourseResource insert(@Parameter(description = "Course to insert. Must have valid fields.",
+            required = true, schema = @Schema(implementation = CourseResource.class))
+                          @Valid @RequestBody CourseResource courseResource) {
         return courseServicePort.insert(courseResource);
     }
 
-    @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
     CourseResource getOne(@PathVariable String id) {
         CourseResource courseResource = courseServicePort.getOne(id);
         if (courseResource != null) {
@@ -48,8 +66,8 @@ public class CourseController {
         }
     }
 
-    @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @GetMapping
     List<CourseResource> getAll() {
         return courseServicePort.getAll();
     }
--- src/main/java/br/com/azusah/greedy/framework/controllers/resources/CourseResource.java
+++ src/main/java/br/com/azusah/greedy/framework/controllers/resources/CourseResource.java
@@ -1,6 +1,7 @@
 package br.com.azusah.greedy.framework.controllers.resources;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,32 +23,72 @@ import java.time.LocalDate;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class CourseResource {
 
+    @Schema(description = "Unique identifier of the Course.",
+            example = "5e56c653ca6a004f9b007151",
+            required = false
+    )
     private String id;
 
+    @Schema(description = "The name of the course.",
+            example = "Spring Boot with OpenApi Documentation. :-P",
+            required = true
+    )
     @NotBlank
     @Size(min = 3)
     private String title;
 
+    @Schema(description = "The way how this course will be offered.",
+            example = "PRESENTIAL",
+            required = true
+    )
     @NotBlank
     private String modality;
 
+    @Schema(description = "Soon description about the course",
+            example = "The course will teach you everything about nothing.",
+            required = false
+    )
     private String description;
 
+    @Schema(description = "Date when the course starts",
+            example = "2020-02-26",
+            required = true
+    )
     @Future
     private LocalDate start;
 
+    @Schema(description = "Date when the course ends",
+            example = "2020-02-26",
+            required = true
+    )
     @Future
     private LocalDate finish;
 
+    @Schema(description = "The instructor's name(s).",
+            example = "Qui-Gon Jinn, Obi Wan Kenobi, Mace Windu",
+            required = true
+    )
     @NotBlank
     private String instructor;
 
+    @Schema(description = "For who is the course.",
+            example = "ANYONE",
+            required = true
+    )
     @NotBlank
     private String audience;
 
+    @Schema(description = "For PRESENTIAL courses, the place where will happen.",
+            example = "A location resource.",
+            required = true
+    )
     @Valid
     private LocationResource location;
 
+    @Schema(description = "For ONLINE courses, the URL where it can be found.",
+            example = "http://udemy.com/id-of-the-course",
+            required = true
+    )
     @URL
     private String url;
 
