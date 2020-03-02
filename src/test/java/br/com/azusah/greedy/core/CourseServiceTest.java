9ec0be7e21daa5e1376d92b22763fb942ea476ca
Daniel Luís Benedito Albino <daniel.albino@gmail.com>
2020-03-01 23:06:55 -0300

    Adding class for tests on core business rules.

---
+++ src/test/java/br/com/azusah/greedy/core/CourseServiceTest.java
@@ -0,0 +1,26 @@
+package br.com.azusah.greedy.core;
+
+import org.junit.jupiter.api.Test;
+
+class CourseServiceTest {
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void getOne() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteInALogicalWay() {
+    }
+}
