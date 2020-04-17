ce06d0fb512b88c535aa156b55ba6d2c1dcb83d5
Daniel Albino <daniel.albino@gmail.com>
2020-04-17 15:47:18 -0300

    Reformating code on CourseController.

--- src/main/java/br/com/azusah/greedy/framework/controllers/CourseController.java
+++ src/main/java/br/com/azusah/greedy/framework/controllers/CourseController.java
@@ -35,12 +35,12 @@ public class CourseController {
     private final ICourseServicePort courseServicePort;
 
     @Operation(summary = "Saves a resource on database and returns it.",
-        description = "Given a valid resource saves it on database and returns the object saved.",
-        tags = {"contact"})
+            description = "Given a valid resource saves it on database and returns the object saved.",
+            tags = {"contact"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "resource created on database",
-            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResponse.class)))),
-        @ApiResponse(responseCode = "400", description = "Some data on resource body is invalid.")
+            @ApiResponse(responseCode = "201", description = "resource created on database",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResponse.class)))),
+            @ApiResponse(responseCode = "400", description = "Some data on resource body is invalid.")
     })
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
@@ -53,12 +53,12 @@ public class CourseController {
     }
 
     @Operation(summary = "Returns a resource saved on database.",
-        description = "Given a valid id, returns a resource saved on database.",
-        tags = {"contact"})
+            description = "Given a valid id, returns a resource saved on database.",
+            tags = {"contact"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Resource found on database",
-            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResponse.class)))),
-        @ApiResponse(responseCode = "404", description = "Resource not found with given id.")
+            @ApiResponse(responseCode = "200", description = "Resource found on database",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResponse.class)))),
+            @ApiResponse(responseCode = "404", description = "Resource not found with given id.")
     })
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
@@ -77,11 +77,11 @@ public class CourseController {
     }
 
     @Operation(summary = "Returns a list of resources saved on database.",
-        description = "Just returns a list of resources saved on database.",
-        tags = {"contact"})
+            description = "Just returns a list of resources saved on database.",
+            tags = {"contact"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "List of resources",
-            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResponse.class))))
+            @ApiResponse(responseCode = "200", description = "List of resources",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResponse.class))))
     })
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
@@ -94,13 +94,13 @@ public class CourseController {
     }
 
     @Operation(summary = "Updates a resource on database.",
-        description = "Given a valid id, and a valid body, updates the object on database and returns it.",
-        tags = {"contact"})
+            description = "Given a valid id, and a valid body, updates the object on database and returns it.",
+            tags = {"contact"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful updated ",
-            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResponse.class)))),
-        @ApiResponse(responseCode = "400", description = "Some data on resource body is invalid."),
-        @ApiResponse(responseCode = "404", description = "Resource not found with given id.")
+            @ApiResponse(responseCode = "200", description = "successful updated ",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseResponse.class)))),
+            @ApiResponse(responseCode = "400", description = "Some data on resource body is invalid."),
+            @ApiResponse(responseCode = "404", description = "Resource not found with given id.")
     })
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
@@ -115,11 +115,11 @@ public class CourseController {
     }
 
     @Operation(summary = "Returns a message of success with id of the deleted object.",
-        description = "Given a valid id, returns a message of success on deleting processes.",
-        tags = {"contact"})
+            description = "Given a valid id, returns a message of success on deleting processes.",
+            tags = {"contact"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "resource deleted on database"),
-        @ApiResponse(responseCode = "404", description = "Resource not found with given id.")
+            @ApiResponse(responseCode = "200", description = "resource deleted on database"),
+            @ApiResponse(responseCode = "404", description = "Resource not found with given id.")
     })
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
